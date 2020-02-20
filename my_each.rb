4e0c8a95040affdc81375f5f5f54e6e713fda2f3
Alexia Adediran <alexiaadediran30@gmail.com>
2020-02-20 12:48:57 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,6 +1,6 @@
-# def my_each # put argument(s) here
-#   # code here
-# end 
+def my_each # put argument(s) here
+  # code here
+end 
 # def my_each(array) # put argument(s) here
 #   # code here
 # end 
@@ -15,15 +15,15 @@
 # my_each(collection) do |i|
 #   puts i
 # end
-class Array
-  def for_each
-    counter = 0
-    while counter < self.size do
-      yield(self[counter]) # pass argument to block and execute 
-      counter += 1
-    end
-    self     # original array is returned 
-  end
-end
-arr = [1, 2, 3, 4]
-arr.for_each {|num| p num }
+# class Array
+#   def for_each
+#     counter = 0
+#     while counter < self.size do
+#       yield(self[counter]) # pass argument to block and execute 
+#       counter += 1
+#     end
+#     self     # original array is returned 
+#   end
+# end
+# arr = [1, 2, 3, 4]
+# arr.for_each {|num| p num }
